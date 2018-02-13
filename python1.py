0bedde26143e210c3cccb2f0ee43a8ed729baad1
udirbetalab <35842072+udirbetalab@users.noreply.github.com>
2018-02-13 11:12:02 +0100

    Update python1.py

--- python1.py
+++ python1.py
@@ -11,7 +11,7 @@ from auth_xxxx import (
 )
 
 tweet_number = 0
-stringToTrack = "denmark"
+stringToTrack = "#trump" #change to what you want
 
 api = TwitterAPI(consumer_key, consumer_secret, access_token, access_token_secret)
 print 'Twitter ready!'
@@ -20,8 +20,8 @@ s = api.request('statuses/filter', {'track':stringToTrack})
 
 for item in s:
         if "text" in item:
-          print item['user']['screen_name'].encode('utf-8') + ' tweeted: ' + item['text'].encode('utf-8')
-          tweet_number = tweet_number + 1
-          print(tweet_number)
-          print("--------")
-          print("")
+            tweet_number = tweet_number + 1
+            print("Tweet number: " + str(tweet_number))
+            print item['user']['screen_name'].encode('utf-8') + ' tweeted: ' + item['text'].encode('utf-8')
+            print("--------")
+            print("")
