f59c1dc6aab997ce85cfe8f7440a1aee24469bec
udirbetalab <35842072+udirbetalab@users.noreply.github.com>
2018-02-13 11:14:03 +0100

    Create auth_xxxx.py

---
+++ auth_xxxx.py
@@ -0,0 +1,6 @@
+#make keys at https://apps.twitter.com/app/new (you need to log in with Twitter account)
+
+consumer_key        = 'your_key'
+consumer_secret     = 'your_key'
+access_token        = 'your_key'
+access_token_secret = 'your_key'
